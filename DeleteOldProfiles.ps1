00d5f06b5b5adacf5a5ccfa290f60e86cc02cba9
renaico <moralesrene3@gmail.com>
2021-06-04 18:24:21 -0400

    Script para borrar perfiles V2

--- DeleteOldProfiles.ps1
+++ DeleteOldProfiles.ps1
@@ -1,12 +1,15 @@
 
+    # Habilitacion de la ejecucion de scripts
+    set-executionpolicy remotesigned
     # La siguiente linea define los perfiles que nunca se eliminaran
     $ExcludedUsers ="Public","zenoss","svc","administrator","administrador"
+    #Filtrado de Perfiles
     $LocalProfiles=Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and (!$_.Loaded) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-1))}
     foreach ($LocalProfile in $LocalProfiles)
     {
     if (!($ExcludedUsers -like $LocalProfile.LocalPath.Replace("C:\Users\","")))
     {
     $LocalProfile | Remove-WmiObject
-    Write-host $LocalProfile.LocalPath, "profile deleted” -ForegroundColor Magenta
+    Write-host $LocalProfile.LocalPath, "profile deleted" -ForegroundColor Magenta
     }
     } 
