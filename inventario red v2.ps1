23c43dd7aad25cce83ec6f05d3e29a8bf769f181
Rene Morales Bastias <moralesrene3@gmail.com>
2022-07-23 01:29:58 -0400

    invebtario equipos windows

---
+++ inventario red v2.ps1
@@ -0,0 +1,19 @@
+﻿$computername = hostname
+
+$origen = "$env:TEMP"
+
+$destino = "\\192.168.10.110\shares\inventario_computadores"+'\'+$computername+'.csv' 
+
+$direccion = (Resolve-DnsName -Type A $computername | Select-Object IPAddress | ft -HideTableHeaders)
+
+Get-ComputerInfo | Select-Object WindowsProductName,WindowsRegisteredOrganization,WindowsRegisteredOwner,BiosBIOSVersion,BiosManufacturer,CsDNSHostName,CsDomain,CsDomainRole,CsManufacturer,CsModel,CsName,CsNumberOfLogicalProcessors,CsNumberOfProcessors,CsProcessors,CsPrimaryOwnerName,CsSystemFamily,CsSystemSKUNumber,CsSystemType,CsTotalPhysicalMemory,CsPhyicallyIns,talledMemory,CsUserName,CsWakeUpType,CsWorkgroup,OsName,OsHotFixes,OsBootDevice,OsSystemDevice,OsSystemDirectory,OsSystemDrive,OsWindowsDirectory,OsCountryCode,OsCurrentTimeZone,OsLocaleID,OsLocale,OsLocalDateTime,OsLastBootUpTime,OsUptime,OsTotalVisibleMemorySize,OsFreePhysicalMemory,OsTotalVirtualMemorySize,OsFreeVirtualMemory,OsInUseVirtualMemory,OsTotalSwapSpaceSize,OsSizeStoredInPagingFiles,OsFreeSpaceInPagingFiles,OsNumberOfUsers,OsOrganization,OsArchitecture,OsLanguage,OsProductSuites,OsProductType,OsRegisteredUser,OsSerialNumber,KeyboardLayout,TimeZone,LogonServer | Export-Csv -Path $origen\file.csv
+
+
+$datos = (Import-Csv -Path $origen\file.csv -Delimiter ',' -Encoding Default)
+
+foreach ($dato in $datos){
+    Add-Member -InputObject $dato -MemberType NoteProperty -Name "IpAddress" -Value $direccion
+}
+
+
+Copy-Item -Path $origen\file.csv -Destination $destino -Force 
---
+++ inventario_red.ps1
@@ -0,0 +1,13 @@
+﻿$computername = hostname
+
+$origen = "%TEMP%"
+
+#$destino = "\\192.168.10.110\shares\inventario_computadores"+'\'+$computername+'.csv'
+
+$direccion = (Resolve-DnsName -Type A $computername | Select-Object IPAddress)
+
+$computerinfo = (Get-ComputerInfo | Select-Object WindowsProductName,WindowsRegisteredOrganization,WindowsRegisteredOwner,BiosBIOSVersion,BiosManufacturer,CsDNSHostName,CsDomain,CsDomainRole,CsManufacturer,CsModel,CsName,CsNumberOfLogicalProcessors,CsNumberOfProcessors,CsProcessors,CsPrimaryOwnerName,CsSystemFamily,CsSystemSKUNumber,CsSystemType,CsTotalPhysicalMemory,CsPhyicallyIns,talledMemory,CsUserName,CsWakeUpType,CsWorkgroup,OsName,OsHotFixes,OsBootDevice,OsSystemDevice,OsSystemDirectory,OsSystemDrive,OsWindowsDirectory,OsCountryCode,OsCurrentTimeZone,OsLocaleID,OsLocale,OsLocalDateTime,OsLastBootUpTime,OsUptime,OsTotalVisibleMemorySize,OsFreePhysicalMemory,OsTotalVirtualMemorySize,OsFreeVirtualMemory,OsInUseVirtualMemory,OsTotalSwapSpaceSize,OsSizeStoredInPagingFiles,OsFreeSpaceInPagingFiles,OsNumberOfUsers,OsOrganization,OsArchitecture,OsLanguage,OsProductSuites,OsProductType,OsRegisteredUser,OsSerialNumber,KeyboardLayout,TimeZone,LogonServer)
+
+#Copy-Item -Path $origen\*.* -Destination $destino -Force 
+
+Write-Host $computerinfo + $direccion | Export-Csv -Path $origen\file.csv
